8c2627ab55119318d9bcac59de1a8ab0a4d3c165
lukasilverio94 <lukasilverio94@gmail.com>
2024-02-26 23:08:49 +0100

    Task detail by id displayed on basic template

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
+
+# All Tasks
 
 
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
+
+# Detail View
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'todo_list/task.html'
